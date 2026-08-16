ba12ffd548d4820ccd1211a65f9b8e675f557533
BishopCodes <BishopCodes@outlook.com>
2026-08-16 12:56:37 -0400

    feature: analytics with umami

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
+import { remarkModified } from "@/plugins/remark-modified.mjs";
+import { remarkReadingTime } from '@/plugins/remark-reading-time.mjs';
 import { unified } from '@astrojs/markdown-remark';
+import tailwindcss from '@tailwindcss/vite';
+import umami from "@yeskunall/astro-umami";
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
-import tailwindcss from '@tailwindcss/vite';
-import { remarkModified } from "@/plugins/remark-modified.mjs"
-import { remarkReadingTime } from '@/plugins/remark-reading-time.mjs';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -24,6 +25,5 @@ export default defineConfig({
       remarkPlugins: [remarkModified, remarkReadingTime]
     })
   },
-
-  integrations: [sitemap()]
+  integrations: [sitemap(), umami({ id: "a3d374e3-89d8-4950-bd77-49ee88887ab7" })]
 });
